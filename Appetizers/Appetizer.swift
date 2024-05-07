020df8b93eb6ae6a339185b887b690d30423a7d0
Pinheiro-dev <mathepba@gmail.com>
2024-05-07 19:11:56 -0300

    Cria model e mock

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,43 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Matheus Pinheiro on 07/05/24.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let samppleAppetizer = Appetizer(
+        id: 0001,
+        name: "Test Appetizer",
+        description: "This is description for my appetizer. It's yummy.",
+        price: 9.99,
+        imageURL: "",
+        calories: 99,
+        protein: 99,
+        carbs: 99
+    )
+    
+    static let appetizers = [
+        samppleAppetizer,
+        samppleAppetizer,
+        samppleAppetizer,
+        samppleAppetizer
+    ]
+}
