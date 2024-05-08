b2630c539e91fe8b4b6d285066672b9c705e0cfd
Pinheiro-dev <mathepba@gmail.com>
2024-05-08 20:45:43 -0300

    Cria lista de appetizers e cria celula

---
+++ Appetizers/AppetizerListCell.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers
+//
+//  Created by Matheus Pinheiro on 08/05/24.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerListCell()
+}
