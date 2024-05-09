76897f4a80da4907c9c94e9172b154c690daedf4
Pinheiro-dev <mathepba@gmail.com>
2024-05-08 21:14:46 -0300

    Cria networkManager e cria model erro

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Matheus Pinheiro on 08/05/24.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
