961ff345c75844c74fb03e87c466ef9008ffb707
Pinheiro-dev <mathepba@gmail.com>
2024-05-14 22:00:08 -0300

    Cria função para baixar imagem e armazenar em cache

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Matheus Pinheiro on 08/05/24.
 //
 
-import Foundation
+import SwiftUI
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -44,6 +45,33 @@ final class NetworkManager {
             } catch {
                 completion(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completion: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completion(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
+            guard let data = data, let image = UIImage(data: data) else {
+                completion(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completion(image)
         }
         
         task.resume()
