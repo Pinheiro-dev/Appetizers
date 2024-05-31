50aee06c38b881b2201b50168dcbdc0db77fcaec
Pinheiro-dev <mathepba@gmail.com>
2024-05-31 18:52:09 -0300

    Cria formulario de conta

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account")
-                .navigationTitle("⚙️ Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)                    
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                } header: {
+                    Text("Personal Info")
+                }
+
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refiils", isOn: $frequentRefills)
+                } header: {
+                    Text("Requests")
+                }
+                    
+            }
+            .tint(.brandPrimary)
+            .navigationTitle("⚙️ Account")
         }
         
     }
